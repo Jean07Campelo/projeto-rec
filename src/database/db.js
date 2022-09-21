e57d6bc9133c72ed3febbef1274125b73977e311
Jean Campelo <jean93campelo@gmail.com>
2022-09-21 09:25:15 -0300

    Conectando com banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+  await mongoClient.connect();
+  console.log("Mongodb connected");
+} catch (error) {
+  console.log(error.message);
+}
+
+const db = mongoClient.db("DrivencracyJ");
+
+export default db;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
 
-import dotenv from "dotenv";
-dotenv.config();
-
 const server = express();
 server.use(cors());
 server.use(express.json());
-const port = 5000;
+//const port = 5000;
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
