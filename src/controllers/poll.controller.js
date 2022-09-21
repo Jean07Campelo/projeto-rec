1e5e2691044b58160245af736894abcd05f274ed
Jean Campelo <jean93campelo@gmail.com>
2022-09-21 10:20:16 -0300

    Recebendo e validando dados do body request

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -1,8 +1,22 @@
 import db from "../database/db.js";
+import joi from "joi";
 
-async function RegisterPoll (req, res) {
+const pollShema = joi.object({
+  title: joi.string().required().empty(),
+  expireAt: joi.string().required(),
+});
 
+async function RegisterPoll(req, res) {
+  const { title, expireAt } = req.body;
+
+  const validationPoll = pollShema.validate(req.body, { abortEarly: false });
+
+  if (validationPoll.error) {
+    const errors = validationPoll.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  res.sendStatus(201);
 }
-
 
 export { RegisterPoll };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,3 +5,5 @@ import * as pollController from "../controllers/poll.controller.js";
 const router = express.Router();
 
 router.post("/poll", pollController.RegisterPoll);
+
+export default router;
