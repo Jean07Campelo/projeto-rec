514ced5ff74cfb1120d53ca1479c96b4c63cd93d
Jean Campelo <jean93campelo@gmail.com>
2022-09-22 15:18:08 -0300

    Validando title e pollId recebidos pelo body da request

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -1,7 +1,26 @@
 import db from "../database/db.js";
+import joi from "joi";
+
+const choiceSchema = joi.object({
+  title: joi.string().required().empty(" "),
+  pollId: joi.string().required(),
+});
 
 async function RegisterOption(req, res) {
+  const { title, pollId } = req.body;
 
+  const validationChoice = choiceSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validationChoice.error) {
+    const errors = validationChoice.error.details.map(
+      (detail) => detail.message
+    );
+    return res.status(422).send(errors);
+  }
+
+  res.sendStatus(201);
 }
 
-export { RegisterOption }
+export { RegisterOption };
