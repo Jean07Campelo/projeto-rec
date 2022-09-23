f8ad952ca4f7f24c011efe10d2478d3252685d4b
Jean Campelo <jean93campelo@gmail.com>
2022-09-23 14:51:14 -0300

    Verificando se pollId passado é válido

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -1,5 +1,6 @@
 import db from "../database/db.js";
 import joi from "joi";
+import { ObjectId } from "mongodb";
 
 const choiceSchema = joi.object({
   title: joi.string().required().empty(" "),
@@ -18,6 +19,19 @@ async function RegisterOption(req, res) {
       (detail) => detail.message
     );
     return res.status(422).send(errors);
+  }
+
+  //validation poll existing by _id
+  try {
+    const pollExisting = await db
+      .collection("polls")
+      .findOne({ _id: ObjectId(pollId) });
+
+    if (!pollExisting) {
+      return res.status(400).send("pollId is not existing");
+    }
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
   }
 
   res.sendStatus(201);
