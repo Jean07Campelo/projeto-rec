1a23a68e934605d5d31cc325a9ce3d2f72b4ab63
Jean Campelo <jean93campelo@gmail.com>
2022-09-24 12:14:44 -0300

    Mensagem de erro quando não existir enquete vinculada ao id passado na rota

--- src/controllers/result.controller.js
+++ src/controllers/result.controller.js
@@ -1,6 +1,24 @@
-function GetResultByPollId(req, res) {
-    
-    res.sendStatus(200);
+import db from "../database/db.js";
+import { ObjectId } from "mongodb";
+
+async function GetResultByPollId(req, res) {
+  const pollId = req.params.id;
+
+  try {
+    const pollSeleted = await db
+      .collection("polls")
+      .findOne({ _id: ObjectId(pollId) });
+
+    if (!pollSeleted) {
+      return res
+        .status(404)
+        .send(`Don't exist poll registered with id: ${pollId}`);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+
+  res.sendStatus(200);
 }
 
 export { GetResultByPollId };
