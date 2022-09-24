6c402d78993540ddb0fb1023cebc32b8b637f106
Jean Campelo <jean93campelo@gmail.com>
2022-09-24 11:13:17 -0300

    Verificação se voto a ser registrado percente a uma enquete expirada

--- src/controllers/vote.controller.js
+++ src/controllers/vote.controller.js
@@ -1,17 +1,44 @@
 import db from "../database/db.js";
 import { ObjectId } from "mongodb";
 import dayjs from "dayjs";
+import compareTime from "dayjs/plugin/isSameOrBefore.js";
+
+const isSameOrBefore = compareTime;
+dayjs.extend(isSameOrBefore);
 
 async function RegisterNewVote(req, res) {
   const choiceId = req.params.id;
 
   try {
+    //search choice by id
     const choiceSelected = await db
       .collection("choices")
       .findOne({ _id: ObjectId(choiceId) });
 
     if (!choiceSelected) {
       return res.status(404).send(`Choice with id "${choiceId}" is invalid`);
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+
+  try {
+    //search poll and validate expire date
+    const pollSelected = await db
+      .collection("polls")
+      .findOne({ _id: ObjectId(choiceSelected.pollId) });
+
+    const timeNow = dayjs(Date.now()).format("YYYY-MM-DD HH:mm");
+    const pollExpired = dayjs(pollSelected.expireAt).isSameOrBefore(
+      dayjs(timeNow)
+    );
+
+    if (pollExpired) {
+      return res
+        .status(403)
+        .send(
+          `The poll selected "${pollSelected.title}" expired in ${pollSelected.expireAt}`
+        );
     }
   } catch (error) {
     return res.status(500).send(error.message);
