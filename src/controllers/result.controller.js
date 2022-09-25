6c11a1edf467b20e9d940d3ca8dbd96fd66af284
Jean Campelo <jean93campelo@gmail.com>
2022-09-25 12:14:06 -0300

    Retornando objeto com dados da enquete e voto vencedor

--- src/controllers/result.controller.js
+++ src/controllers/result.controller.js
@@ -14,44 +14,55 @@ async function GetResultByPollId(req, res) {
         .status(404)
         .send(`Don't exist poll registered with id: ${pollId}`);
     }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
 
-  try {
     const registeredChoices = await db
       .collection("choices")
       .find({ pollId: ObjectId(pollId) })
       .toArray();
 
     //save id from all choices registered
-    const choicesId = registeredChoices.map((item) => (item._id).toString());
+    const choicesId = registeredChoices.map((item) => item._id.toString());
 
     const votes = await db.collection("votes").find().toArray();
-    const votesId = votes.map((item) => (item.choiceId).toString())
-      
+    const votesId = votes.map((item) => item.choiceId.toString());
+
     //calculates choice most voted
     let mostVotedQuantity = 0;
     let choiceMostVoted = "";
-      choicesId.forEach(counterVotes);
-      function counterVotes(choice) {
-        let count = 0;
-        for (let i = 0; i < votesId.length; i++) {
-          if (choice === votesId[i]) {
-            count++;
-          }
-        }
-        if (count >= mostVotedQuantity) {
-          mostVotedQuantity = count;
-          choiceMostVoted = choice;
+    choicesId.forEach(counterVotes);
+    function counterVotes(choice) {
+      let count = 0;
+      for (let i = 0; i < votesId.length; i++) {
+        if (choice === votesId[i]) {
+          count++;
         }
       }
+      if (count >= mostVotedQuantity) {
+        mostVotedQuantity = count;
+        choiceMostVoted = choice;
+      }
+    }
 
+    const winningChoice = registeredChoices.find(
+      (choice) => choice._id.toString() === choiceMostVoted
+    );
+
+    const result = {
+      title: winningChoice.title,
+      votes: mostVotedQuantity,
+    };
+
+    const resultPoll = {
+      _id: pollId,
+      title: pollSeleted.title,
+      expireAt: pollSeleted.expireAt,
+      result,
+    };
+
+    res.status(200).send(resultPoll);
   } catch (error) {
     res.status(500).send(error.message);
   }
-
-  res.sendStatus(200);
 }
 
 export { GetResultByPollId };
